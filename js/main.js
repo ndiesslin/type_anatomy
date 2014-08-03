5f25b5b35451e8a5fb43889bf60caff35e305267
nicholas diesslin <nicholasdiesslin@gmail.com>
2014-08-03 02:10:21 -0500

    v1.1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+function showletter(letterchosen) {
+     $('.letters').each(function(index) {
+          if ($(this).attr("id") == letterchosen) {
+               $(this).fadeIn(600);
+          }
+          else {
+               $(this).fadeOut(600);
+          }
+     });
+}
