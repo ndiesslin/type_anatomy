3e28d95abb7498ef0d6baef8d53e4e8f0d3cc16f
nicholas diesslin <nicholasdiesslin@gmail.com>
2014-08-04 22:49:52 -0500

    v1.2 adding graphics

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ function showletter(letterchosen) {
      $('.letters').each(function(index) {
           if ($(this).attr("id") == letterchosen) {
                $(this).fadeIn(600);
+               $("#letter-space").css("position", "fixed");
+               $("nav").css("position", "fixed");
           }
           else {
-               $(this).fadeOut(600);
+               $(this).fadeOut(200);
           }
      });
 }
