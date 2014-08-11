f6650f2ea9f83e6493f1a886d0df89cabc347840
nicholas diesslin <nicholasdiesslin@gmail.com>
2014-08-10 23:36:52 -0500

    v1.3 updated link onclick

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 function showletter(letterchosen) {
-     $('.letters').each(function(index) {
-          if ($(this).attr("id") == letterchosen) {
-               $(this).fadeIn(600);
-               $("#letter-space").css("position", "fixed");
-               $("nav").css("position", "fixed");
-          }
-          else {
-               $(this).fadeOut(200);
-          }
-     });
+		$('.letters').each(function(index) {
+		  if ($(this).attr("id") == letterchosen) {
+		       $(this).fadeIn(600);
+		       $("#letter-space").css("position", "fixed");
+		       $("nav").css("position", "fixed");
+		  }
+		  else {
+		       $(this).fadeOut(200);
+		  }
+		});
+		$('.nav_click').click(function(e){
+		    $('.nav_click').css("border-bottom", "1px #00ccff solid");
+		    $(this).css("border-bottom", "1px #ff6950 solid");
+		});
 }
