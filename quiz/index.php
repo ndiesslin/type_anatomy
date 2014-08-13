3068bcd702251d1202c811078046f28f2287b672
nicholas diesslin <nicholasdiesslin@gmail.com>
2014-08-13 00:07:14 -0500

    v1.4 adding and working on quiz

--- quiz/index.php
+++ quiz/index.php
@@ -1,19 +1,22 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	
-	<title>PHP Quiz</title>
-	
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Type Anatomy Quiz</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css" />
+
 </head>
 
 <body>
 
 	<div id="page-wrap">
 
-		<h1>Final Quiz for Lip building</h1>
+		<h1>Type Anatomy</h1>
 		
 		<form action="grade.php" method="post" id="quiz">
 		
@@ -156,16 +159,6 @@
 		</form>
 	
 	</div>
-	
-	<script type="text/javascript">
-	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-	</script>
-	<script type="text/javascript">
-	var pageTracker = _gat._getTracker("UA-68528-29");
-	pageTracker._initData();
-	pageTracker._trackPageview();
-	</script>
 
 </body>
 
