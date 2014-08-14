711a4dc792a21276a28aef34d68a5b759b3a185d
nicholas diesslin <nicholasdiesslin@gmail.com>
2014-08-13 22:15:57 -0500

    v1.4 adding and working on quiz

--- quiz/grade.php
+++ quiz/grade.php
@@ -36,16 +36,6 @@
         ?>
 	
 	</div>
-	
-	<script type="text/javascript">
-	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-	</script>
-	<script type="text/javascript">
-	var pageTracker = _gat._getTracker("UA-68528-29");
-	pageTracker._initData();
-	pageTracker._trackPageview();
-	</script>
 
 </body>
 
